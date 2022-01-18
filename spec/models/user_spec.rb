86bb515a0681276e230668639ccfbbc0529ee60a
Rostyslav Yemelyanov <emrb.2708@gmail.com>
2022-01-18 14:14:48 +0200

    Add tests and small fixes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
-require 'support/factory_bot'
 
 RSpec.describe User, type: :model do
-  @user1 = User.create(:user)
+  let!(:user) {create(:user)}
   it "is valid" do
-      expect(@user).to be_valid
+      expect(user).to be_valid
   end
 end
